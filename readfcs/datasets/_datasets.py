a54d3eb3d86603415a85066cefdb8323dd06f7de
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-07-10 22:30:39 +0800

    :memo: added docstring to datasets

--- readfcs/datasets/_datasets.py
+++ readfcs/datasets/_datasets.py
@@ -4,4 +4,8 @@ HERE = Path(__file__).parent
 
 
 def example() -> Path:
+    """Example fcs file from FlowIO.
+
+    https://github.com/whitews/FlowIO/blob/master/examples/fcs_files/100715.fcs
+    """
     return HERE / "example.fcs"
