d2cce486a2ea00998d9d2bcd00253b7d51877997
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-01 23:47:38 +0800

    :sparkles: Added a read function

--- readfcs/datasets/_datasets.py
+++ readfcs/datasets/_datasets.py
@@ -1,11 +1,6 @@
-from pathlib import Path
-
-HERE = Path(__file__).parent
-
-
-def example() -> Path:
+def example() -> str:
     """Example fcs file from FlowIO.
 
     https://github.com/whitews/FlowIO/blob/master/examples/fcs_files/100715.fcs
     """
-    return HERE / "example.fcs"
+    return "https://lamindb-test.s3.amazonaws.com/example.fcs"
