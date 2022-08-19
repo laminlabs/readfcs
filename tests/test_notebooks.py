c82d6a3bb44e0191fff60d89d730260226b748e0
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-19 12:00:08 +0200

    :green_heart: fix nb checks

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -9,8 +9,8 @@ def test_notebooks():
     docs_folder = Path(__file__).parents[1] / "docs/"
 
     for check_folder in docs_folder.glob("./**"):
-        # these are the typical notebook testpaths
-        if not str(check_folder).endswith(("guides", "tutorials")):
-            continue
-        logger.info(f"\n---{check_folder.stem}---")
+        # # these are the typical notebook testpaths
+        # if not str(check_folder).endswith(("guides", "tutorials")):
+        #     continue
+        logger.info(f"\n---{check_folder}---")
         test.execute_notebooks(check_folder, write=True)
