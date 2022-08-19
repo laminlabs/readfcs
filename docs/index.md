6df9158b339bc34c58a0397eeba699890ca71103
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-19 18:14:14 +0800

    📝 Make the tutorials executed (#10)
    * :white_check_mark: Execute tutorial nb
    
    * :memo: Execute the nbs
    
    * :memo: Execute the nbs
    
    * :technologist: import lamin_logger
    
    * :truck: moved the quickstart to the root
    
    * :memo: updated index
    
    * :memo: updated index
    
    * :green_heart: updated index
    
    * :memo: cleaned up quickstart
    
    * :green_heart: fix nb checks
    
    * :green_heart: fix nb checks
    
    * :green_heart: fix tutorial
    
    * :green_heart: fix link

--- docs/index.md
+++ docs/index.md
@@ -1,18 +1,16 @@
 ```{include} ../README.md
 :start-line: 0
-:end-line: 3
+:end-line: 4
 ```
 
-- Get started with [Quickstart](guides/quickstart).
-- Check out the [API](api).
-- Learn about design decisions and comparisons to other tools: [notes](notes/index).
+- Get started with [Quickstart](quickstart).
+- Check out the [API](api) reference.
 
 ```{toctree}
 :maxdepth: 1
 :hidden:
 
-guides/index
+quickstart
 api
-notes/index
 changelog
 ```
---
+++ tests/test_notebooks.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from lamin_logger import logger
+from nbproject.dev import test
+
+
+def test_notebooks():
+    # assuming this is in the tests folder
+    docs_folder = Path(__file__).parents[1] / "docs/"
+
+    for check_folder in docs_folder.glob("./**"):
+        # # these are the typical notebook testpaths
+        # if not str(check_folder).endswith(("guides", "tutorials")):
+        #     continue
+        logger.info(f"\n---{check_folder}---")
+        test.execute_notebooks(check_folder, write=True)
