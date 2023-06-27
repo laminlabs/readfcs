76a7aaaeabdef92904b66a34dbc257e14a6ffda3
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-27 08:27:02 -0400

    🩹 Remove whitespace only markers (#26)

--- readfcs/_core.py
+++ readfcs/_core.py
@@ -206,8 +206,9 @@ class ReadFCS:
         # use channels for non-marker channels
         if reindex:
             adata.var = adata.var.reset_index()
+            adata.var.replace({" ", ""}, inplace=True)
             adata.var.index = np.where(
-                adata.var["marker"].isin(["", " "]),
+                adata.var["marker"] == "",
                 adata.var["channel"],
                 adata.var["marker"],
             )
