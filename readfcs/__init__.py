592430fb700b8837680e421bf1bdd366b88f3db5
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-07-07 11:07:17 +0200

    🔖 Release 1.1.4

--- readfcs/__init__.py
+++ readfcs/__init__.py
@@ -15,7 +15,7 @@ This is the complete API reference:
    datasets
 """
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 # prints warning of python versions
 from lamin_logger import py_version_warning
