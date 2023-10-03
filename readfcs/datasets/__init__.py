01aa73b2a7e6b8da2590a6f9369b9af306559e56
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-10-03 19:36:24 +0200

    🍱 Added oetjen18 files

--- readfcs/datasets/__init__.py
+++ readfcs/datasets/__init__.py
@@ -3,7 +3,10 @@
 .. autosummary::
    :toctree: .
 
+   Oetjen18_t1
+   Oetjen18_t2
+   Oetjen18_dc
    example
 """
 
-from ._datasets import example
+from ._datasets import example, Oetjen18_t1, Oetjen18_t2, Oetjen18_dc
